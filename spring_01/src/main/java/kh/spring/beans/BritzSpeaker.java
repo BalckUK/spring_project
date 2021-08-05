fd7d9f4258ef39e172b0786e74949601854dd4b6
UMK <haga5032@naver.com>
2021-08-05 16:14:46 +0900

    DI 구현

---
+++ spring_01/src/main/java/kh/spring/beans/BritzSpeaker.java
@@ -0,0 +1,9 @@
+package kh.spring.beans;
+
+import kh.spring.interfaces.Speaker;
+
+public class BritzSpeaker implements Speaker {
+	public BritzSpeaker() {
+		System.out.println("Britz 스피커 생성");
+	}
+}
--- spring_01/src/main/java/kh/spring/beans/LgTV.java
+++ spring_01/src/main/java/kh/spring/beans/LgTV.java
@@ -1,24 +1,29 @@
 package kh.spring.beans;
 
+import kh.spring.interfaces.Speaker;
 import kh.spring.interfaces.Tv;
 
 public class LgTV implements Tv {
-	public LgTV() {
-		System.out.println("LG TV 생성!");
-	}
+
+	private Speaker speaker;
 	
+	public LgTV(Speaker speaker) {
+		System.out.println("LG TV 생성!");
+		this.speaker = speaker;
+	}
+
 	public void powerOn() {
 		System.out.println("TV 켜짐");
 	}
-	
+
 	public void powerOff() {
 		System.out.println("TV 꺼짐");
 	}
-	
+
 	public void init() {
 		System.out.println("LG TV 초기 설정 가동");
 	}
-	
+
 	public void destory() {
 		System.out.println("LG TV 고장");
 	}
