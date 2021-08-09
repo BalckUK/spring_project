74d32ee2b040cd40392bfb625ca40296da7c112a
UMK <haga5032@naver.com>
2021-08-09 17:52:15 +0900

    dbcp 구현 완료

--- Spring_03/src/main/java/kh/spring/controller/HomeController.java
+++ Spring_03/src/main/java/kh/spring/controller/HomeController.java
@@ -1,28 +1,44 @@
 package kh.spring.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kh.spring.dao.MessagesDAO;
+import kh.spring.dto.MessagesDTO;
+
 @Controller
 public class HomeController {
+
+	@Autowired
+	private MessagesDAO mdao;
+
 	@RequestMapping("/")
 	public String home() {
 		return "home";
 	}
-	
+
 	@RequestMapping("input")
 	public String toInput() {
 		return "message/input";
 	}
-	
+
+	/*
+	 * @RequestMapping("inputProc") public String inputProc(HttpServletRequest
+	 * request) { String writer = request.getParameter("writer"); String message =
+	 * request.getParameter("message"); System.out.println(writer + " : "+ message);
+	 * return "home"; }
+	 */
+
 	@RequestMapping("inputProc")
-	public String inputProc(HttpServletRequest request) {
-		String writer = request.getParameter("writer");
-		String message = request.getParameter("message");
-		System.out.println(writer + " : "+ message);
-		return "home";
+	public String inputProc(MessagesDTO dto) {
+		try {
+			int result = mdao.insert(dto);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "error";
+		}
+		return "redirect:/";
 	}
 
 }
